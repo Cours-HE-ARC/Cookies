6a882213dc3a79ebf78eb3a28116263afc07b30d
SCE <sce@gglobaz.ch>
2018-09-28 12:13:56 +0200

    initial commit

---
+++ src/ch/hearc/servlet/controlleur/MainServlet.java
@@ -0,0 +1,31 @@
+package ch.hearc.servlet.controlleur;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet(
+		name="mainServlet",
+		urlPatterns = ""
+)
+public class MainServlet extends HttpServlet{
+
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		
+		Cookie cookie = new Cookie("monCookie", "cookie-ok.");
+		cookie.setMaxAge(100 * 1000 * 60); //100 secondes
+		resp.addCookie(cookie);
+		
+	}
+}
